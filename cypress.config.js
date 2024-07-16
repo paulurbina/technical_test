3934630959c686bcbcca5d2636b6039a60ec8d6c
paulurbina <paulurbian@hotmail.com>
2024-07-15 23:44:54 -0500

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,21 @@
-module.exports = {
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
-  },
-};
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: true,
+      charts: true,
+      reportPageTitle: 'Cypress Test Report',
+      embeddedScreenshots: true,
+      inlineAssets: true
+    }
+  }
+});
